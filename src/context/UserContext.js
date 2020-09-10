8eedcaa1e28c01dbf54d41c417c4e6dbd0f7c72d
Chris Mendes <chris@chrismendes.uk>
2020-09-10 17:37:47 +0100

    Removed redundant code

--- src/context/UserContext.js
+++ src/context/UserContext.js
@@ -1,19 +1,6 @@
 import { createContext } from 'react';
 import formations from '../data/formations';
 
-// export const UserFormationContext = createContext({
-//   selection: '442'
-// });
-
-
-// export const UserPlayersContext = createContext({
-//   'gk':  [],
-//   'def': [],
-//   'mid': [],
-//   'fwd': []
-
-// });
-
 const UserTeamContext = createContext({
   formation: formations[0],
   players: {
