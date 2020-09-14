33bbeb7e7345d97637b825bf9daa73fc3633522b
Chris Mendes <chris@chrismendes.uk>
2020-09-14 02:43:56 +0100

    Tweaked style of site/page header

--- src/components/common/AppHeader/AppHeader.js
+++ src/components/common/AppHeader/AppHeader.js
@@ -2,7 +2,9 @@ import React from 'react';
 import './AppHeader.scss';
 
 const AppHeader = () => (
-  <h1>Football Dream Team - Built with React</h1>
+  <div className="appheader">
+    <h1 className="appheader__title">Football Dream Team - Built with React</h1>
+  </div>
 );
 
 export default AppHeader;
