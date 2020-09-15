cbe413cd2985660288c5fc388560a0631493a0d6
Chris Mendes <chris@chrismendes.uk>
2020-09-15 03:22:12 +0100

    Improved player on pitch hover/draggable style

--- src/components/Pitch/Pitch.js
+++ src/components/Pitch/Pitch.js
@@ -15,10 +15,10 @@ const Pitch = () => {
         <li></li>
         <li></li>
         <li className="is-occupied" draggable="true">
-          <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p182539.png" />
-          <span>Pierre-Emerick Aubameyang</span>
+          <img className="pitch__playerimage" src="https://resources.premierleague.com/premierleague/photos/players/250x250/p182539.png" alt="" />
+          <span className="pitch__playername">Pierre-Emerick Aubameyang</span>
         </li>
-        <li></li>
+        <li className="is-highlighted"></li>
         <li></li>
         <li></li>
         <li></li>
