1c634c569804a89302f61fd9a8c83e2b48d11c50
Chris Mendes <chris@chrismendes.uk>
2020-09-15 02:52:36 +0100

    Added draggable style to players on pitch

--- src/components/Pitch/Pitch.js
+++ src/components/Pitch/Pitch.js
@@ -14,7 +14,7 @@ const Pitch = () => {
         <li></li>
         <li></li>
         <li></li>
-        <li className="is-occupied">
+        <li className="is-occupied" draggable="true">
           <img src="https://resources.premierleague.com/premierleague/photos/players/250x250/p182539.png" />
           <span>Pierre-Emerick Aubameyang</span>
         </li>
