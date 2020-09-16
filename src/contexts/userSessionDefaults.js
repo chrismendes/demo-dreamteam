baf54befd9b64867a6c1f709e3a4c3db580015b7
Chris Mendes <chris@chrismendes.uk>
2020-09-17 00:47:13 +0100

    Withdrawn redundant commented out code

--- src/contexts/userSessionDefaults.js
+++ src/contexts/userSessionDefaults.js
@@ -3,6 +3,5 @@ import formations from '../data/formations';
 export default {
     appProgress: [ false, false, false ],
     players:     [],
-    // players:     { 'GK':  [], 'DEF': [], 'MID': [], 'FWD': [] },
     formation:   formations[0]
 };
