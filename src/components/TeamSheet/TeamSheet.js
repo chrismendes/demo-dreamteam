f93e934c0f8502770199e165e2eb1ac2e9275dfc
Chris Mendes <chris@chrismendes.uk>
2020-09-18 16:36:29 +0100

    Removed redundant commented out code

--- src/components/TeamSheet/TeamSheet.js
+++ src/components/TeamSheet/TeamSheet.js
@@ -1,5 +1,4 @@
 import React, { useContext } from 'react';
-// import React from 'react';
 import SelectableCard from '../common/SelectableCard';
 import UserSessionContext from '../../contexts/UserSessionContext';
 
