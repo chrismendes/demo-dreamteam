e3dc8d64ba36c6d83eed67263746b40b30ff6326
Chris Mendes <chris@chrismendes.uk>
2020-09-28 01:58:20 +0100

    Improved appheader style and added github link

--- src/components/common/AppHeader/AppHeader.js
+++ src/components/common/AppHeader/AppHeader.js
@@ -3,7 +3,12 @@ import './AppHeader.scss';
 
 const AppHeader = () => (
   <div className="appheader">
+    <span className="appheader__warning">App is not responsive. Desktop only.</span>
     <h1 className="appheader__title">Football Dream Team - Built with React</h1>
+    <a className="appheader__github" href="https://github.com/chrismendes/app-dreamteam-react" target="_blank" rel="noopener noreferrer">
+      <img src="/github.png" width="32" height="32" alt="GitHub" />
+      <span>Click for source code on GitHub</span>
+    </a>
   </div>
 );
 
